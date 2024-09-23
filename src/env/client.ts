85dd63b2ed88a02c8d0aceba2864ff54d1b4119d
Ignacio Garcia Pizales <igarciap@fi.uba.ar>
2024-09-22 21:04:20 -0300

    actualizo variables de entorno

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,11 +1,20 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+/** Las variables de entorno públicas que se pueden
+ *  usar tanto en el cliente como en el servidor
+ */
 export const env = createEnv({
   clientPrefix: "NEXT_PUBLIC_",
   client: {
     NEXT_PUBLIC_API_URL: z.string().url(),
-    // Las variables de entorno públicas que se usan solo en el cliente
+  },
+  onValidationError: (error) => {
+    console.error(
+      "❌ Invalid client environment variables:",
+      error.flatten().fieldErrors,
+    );
+    process.exit(1);
   },
   // eslint-disable-next-line n/no-process-env
   runtimeEnv: process.env,
