36cc42b3f21348ecb479f0b3c03377780635e5f0
Ignacio Garcia Pizales <igarciap@fi.uba.ar>
2024-09-29 19:50:03 -0300

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,10 +3,10 @@ export default function Footer() {
     <footer className="bg-gray-800 p-4 text-center text-white">
       <p className="mb-1">Desarrollado por:</p>
       <ul className="flex flex-col flex-wrap justify-center space-x-4 sm:flex-row">
-        <li>Alejandro Ballardes</li>
+        <li>Alejandro Balladares - 101118</li>
         <li>Ignacio Garcia Pizales - 105043</li>
         <li>Santiago Luis Mazzocato</li>
-        <li>Andres</li>
+        <li>Andrés Carretero</li>
       </ul>
       <p className="mt-4">&copy; 2024 Itil. All rights reserved.</p>
     </footer>
