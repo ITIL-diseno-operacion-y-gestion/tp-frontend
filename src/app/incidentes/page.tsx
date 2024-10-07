4814327e10df735ef1aa0ec41ecb0192addaec26
ignaciano3 <57163642+ignaciano3@users.noreply.github.com>
2024-10-07 21:39:59 +0000

    agrego para q se pueda actualizar

--- src/app/incidentes/page.tsx
+++ src/app/incidentes/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { ButtonActualizar } from "@/components/button-actualizar";
+
 import { TablaIncidentes } from "./_components/tabla-incidentes";
 import { getIncidentesTickets } from "./actions";
 import { columns } from "./columns";
@@ -9,11 +11,17 @@ export default async function IncidentesPage() {
   return (
     <div>
       <h1 className="text-center text-xl font-bold">Incidentes</h1>
-      <Link href="/incidentes/new">
-        <button className="mb-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-          Nuevo incidente
-        </button>
-      </Link>
+      <div className="flex justify-between">
+        <Link href="/incidentes/new">
+          <button className="mb-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+            Nuevo incidente
+          </button>
+        </Link>
+        <div className="flex flex-wrap gap-x-4">
+          <ButtonActualizar path="/incidentes" />
+        </div>
+      </div>
+
       <TablaIncidentes rowData={incidentes} colDefs={columns} />
     </div>
   );
