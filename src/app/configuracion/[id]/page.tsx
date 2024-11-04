c053bb8b309e4d7fff24210c49fd8fc2144e80f7
ignaciano3 <57163642+ignaciano3@users.noreply.github.com>
2024-11-04 21:19:57 +0000

    saco margin q no va

--- src/app/configuracion/[id]/page.tsx
+++ src/app/configuracion/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function ConfiguracionItemPage(props: {
             Editar
           </button>
         </Link>
-        <Link href="/configuracion" className="mt-6">
+        <Link href="/configuracion">
           <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
             Volver
           </button>
