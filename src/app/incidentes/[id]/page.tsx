1126dddff9d1ad3ba9f329466bbe7d1b551cc998
ignaciano3 <57163642+ignaciano3@users.noreply.github.com>
2024-11-04 21:26:03 +0000

    agrego margen

--- src/app/incidentes/[id]/page.tsx
+++ src/app/incidentes/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function IncidentesDetailsPage(props: {
     <div>
       <Title>Incidente {id}</Title>
       <IncidenteView incidente={incidente} />
-      <div className="flex gap-x-4">
+      <div className="mt-6 flex gap-x-4">
         <BorrarItem id={id} action={borrarIncidente} />
         <Link href="/incidentes" className="mt-6">
           <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
