e99fc79e4124b1f6896325abd4ae736b87235e4a
ignacio <igarciap@fi.uba.ar>
2024-11-11 19:44:01 -0300

    hago menos feo el incidente view

--- src/app/incidentes/[id]/_components/incidente-view.tsx
+++ src/app/incidentes/[id]/_components/incidente-view.tsx
@@ -2,56 +2,84 @@ import Link from "next/link";
 
 import { ChipFormaNotificacion } from "@/components/chips/chip-forma-notificacion";
 import { ChipPrioridad } from "@/components/chips/chip-prioridad";
-import { formatDate } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Incidente } from "@/models/incidentes";
 
 export default function IncidenteView({ incidente }: { incidente: Incidente }) {
-  const {
-    id,
-    id_usuario,
-    fecha_de_alta,
-    forma_de_notificacion,
-    usuarios_afectados,
-    servicios_afectados,
-    prioridad,
-    categoria,
-    informacion_adicional,
-    articulos_afectados,
-  } = incidente;
-
   return (
-    <div className="px-4">
-      <p>Id: {id}</p>
-      <p>Usuario: {id_usuario}</p>
-      <p>Fecha de alta: {formatDate(fecha_de_alta)}</p>
-      <div>
-        Forma de notificación:{" "}
-        <ChipFormaNotificacion formaNotificacion={forma_de_notificacion} />
-      </div>
-      <p>Usuarios afectados: {usuarios_afectados}</p>
-      <p>Servicios afectados: {servicios_afectados}</p>
-      <p>
-        Prioridad: <ChipPrioridad prioridad={prioridad} />
-      </p>
-      <p>Categoría: {categoria}</p>
-      <p>Información adicional: {informacion_adicional}</p>
-      <p>Artículos afectados:</p>
-      <ul className="mt-4 space-y-4">
-        {articulos_afectados.length === 0 ? (
-          <p>No hay artículos afectados</p>
-        ) : (
-          articulos_afectados.map((articulo) => (
-            <li key={articulo.id}>
-              <Link
-                href={`/configuracion/${articulo.id}`}
-                className="text-blue-500"
-              >
-                {articulo.nombre}
-              </Link>
-            </li>
-          ))
-        )}
-      </ul>
-    </div>
+    <Card className="mx-auto w-full max-w-3xl">
+      <CardHeader>
+        <CardTitle>Incidente #{incidente.id}</CardTitle>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <h3 className="font-semibold">ID de Usuario</h3>
+            <p>{incidente.id_usuario}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold">Forma de Notificación</h3>
+            <ChipFormaNotificacion
+              formaNotificacion={incidente.forma_de_notificacion}
+            />
+          </div>
+          <div>
+            <h3 className="font-semibold">Reportador</h3>
+            <p>{incidente.reportador}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold">Fecha de Alta</h3>
+            <p>{incidente.fecha_de_alta}</p>
+          </div>
+        </div>
+        <div>
+          <h3 className="font-semibold">Usuarios Afectados</h3>
+          <p>{incidente.usuarios_afectados}</p>
+        </div>
+        <div>
+          <h3 className="font-semibold">Servicios Afectados</h3>
+          <p>{incidente.servicios_afectados}</p>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <h3 className="font-semibold">Prioridad</h3>
+            <ChipPrioridad prioridad={incidente.prioridad} />
+          </div>
+          <div>
+            <h3 className="font-semibold">Categoría</h3>
+            <p>{incidente.categoria}</p>
+          </div>
+        </div>
+        <div>
+          <h3 className="font-semibold">Información Adicional</h3>
+          <p>{incidente.informacion_adicional}</p>
+        </div>
+        <div>
+          <h3 className="font-semibold">Artículos Afectados</h3>
+          <ul className="space-y-4">
+            {incidente.articulos_afectados.length === 0 ? (
+              <p>No hay artículos afectados</p>
+            ) : (
+              incidente.articulos_afectados.map((articulo) => (
+                <li key={articulo.id}>
+                  <Link
+                    href={`/configuracion/${articulo.id}`}
+                    className="text-blue-500"
+                  >
+                    {articulo.nombre}
+                  </Link>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+        <div>
+          <Button variant="secondary" asChild>
+            <Link href={`/incidentes/${incidente.id}/edit`}>Editar</Link>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
---
+++ src/app/incidentes/[id]/edit/page.tsx
@@ -0,0 +1,7 @@
+export default function EditIncidentePage() {
+  return (
+    <>
+      <h1>Edit Incidente</h1>
+    </>
+  );
+}
