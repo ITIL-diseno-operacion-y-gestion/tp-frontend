61bbe05b30225681560398a1c77703049aab6112
ignacio <igarciap@fi.uba.ar>
2024-11-11 17:58:50 -0300

    rezo a dios q compile

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ["recharts", "react-is"],
   experimental: {
     // eslint-disable-next-line n/no-process-env
     typedRoutes: process.env.TURBOPACK ? false : true,
+    esmExternals: "loose",
   },
 };
 
