f6fd9c4e7d902db5bf32dd2105534a23aee0e024
ignacio <igarciap@fi.uba.ar>
2024-11-11 18:50:35 -0300

    dejo los graficos

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["recharts", "react-is"],
   experimental: {
     // eslint-disable-next-line n/no-process-env
     typedRoutes: process.env.TURBOPACK ? false : true,
-    esmExternals: "loose",
   },
 };
 
--- src/app/reportes/_components/problemas-reporte.tsx
+++ src/app/reportes/_components/problemas-reporte.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { Reporte } from "@/models/reportes";
+
+import { Pie, PieChart } from "recharts";
 
 export function ProblemasReporteView({
   problemas,
@@ -12,14 +24,7 @@ export function ProblemasReporteView({
       <p>
         <strong>Estado:</strong>
       </p>
-      {Object.entries(estado).map(([key, value]) => (
-        <ul key={key}>
-          <li>
-            {key}: {value || 0}
-          </li>
-        </ul>
-      ))}
-
+      <ChartEstado estado={estado} />
       <p>
         <strong>Incidente:</strong>
       </p>
@@ -44,3 +49,43 @@ export function ProblemasReporteView({
     </div>
   );
 }
+
+const ChartEstado = ({
+  estado,
+}: {
+  estado: Reporte["problemas"]["estado"];
+}) => {
+  // "detectado" | "analizandose" | "asignado" | "resuelto" | "cerrado"
+  const chartConfig = {
+    detectado: { color: "pink", label: "Planeado" },
+    analizandose: { color: "blue", label: "Encargado" },
+    asignado: { color: "purple", label: "En creación" },
+    resuelto: { color: "red", label: "En prueba" },
+    cerrado: { color: "orange", label: "En almacén" },
+    value: {
+      label: "Cantidad",
+    },
+  } satisfies ChartConfig;
+
+  const data = Object.entries(estado).map(([key, value]) => ({
+    name: key.split(" ").join("_"),
+    value,
+    fill: `var(--color-${key.split(" ").join("_")})`,
+  }));
+
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="aspect-square max-h-[250px]"
+    >
+      <PieChart>
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <ChartLegend
+          content={<ChartLegendContent nameKey="name" />}
+          className="-translate-y-2 flex-wrap gap-2"
+        />
+        <Pie dataKey="value" nameKey="name" innerRadius={30} data={data} />
+      </PieChart>
+    </ChartContainer>
+  );
+};
