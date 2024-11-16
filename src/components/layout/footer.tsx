7afd4332713df44ca508b2a3321805863b7ec553
Alejandro Balladares <ale_12_river@hotmail.com>
2024-11-16 19:56:14 -0300

    Cambio en el footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
       <ul className="flex flex-col flex-wrap justify-center space-x-4 sm:flex-row">
         <li>Alejandro Balladares - 101118</li>
         <li>Ignacio Garcia Pizales - 105043</li>
-        <li>Santiago Luis Mazzocato</li>
-        <li>Andrés Carretero</li>
+        <li>Santiago Ruiz Mazzocato - 1xxxxx</li>
+        <li>Andrés Carretero - 1xxxxx</li>
       </ul>
       <p className="mt-4">&copy; 2024 Itil. All rights reserved.</p>
     </footer>
