cf8e985840c24c701437d1186350645d4690cc2a
ignacio <igarciap@fi.uba.ar>
2024-12-08 20:45:47 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,12 @@ const nextConfig: NextConfig = {
     typedRoutes: process.env.TURBOPACK ? false : true,
     reactCompiler: true,
     serverActions: {
-      allowedOrigins: ["*", "itil-frontend.netlify.app", "localhost:3000"],
+      allowedOrigins: [
+        "*",
+        "itil-frontend.netlify.app",
+        "localhost:3000",
+        "https://itil-frontend.netlify.app",
+      ],
     },
   },
   headers: async () => {
