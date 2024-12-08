0c93c239b97a53e817c42f1a0224c59b76a245db
ignacio <igarciap@fi.uba.ar>
2024-12-08 20:51:38 -0300

    sigo sacando cosas

--- next.config.ts
+++ next.config.ts
@@ -13,27 +13,6 @@ const nextConfig: NextConfig = {
       ],
     },
   },
-  headers: async () => {
-    return [
-      {
-        source: "*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://itil-frontend.netlify.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
